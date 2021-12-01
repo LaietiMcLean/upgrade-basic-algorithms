6baf3219b0a23ebf0dc6397085b7c17fb2f7daae
LaietiMcLean <laia.marco@bootcamp-upgrade.com>
2021-12-01 10:09:09 +0100

    Iteration#2

--- basic-algorithms.js
+++ basic-algorithms.js
@@ -6,9 +6,34 @@ var y = 10;
 var z = h + y;
 
 //Iteración 2
-const character = {
+    //2.1
+    const character = {
     name: 'Jack Sparrow', 
     age: 10 
-};
+    };
 
-character.age = 25;
+    character.age = 25;
+
+    //2.2
+    var firstName = 'Jon'; 
+    var lastName = 'Snow'; 
+    var age = 24;
+
+    texto = 'Soy ' + firstName + ' ' + lastName + ', tengo ' + age + ' años y me gustan los lobos.';
+    console.log(texto);
+
+    //2.3
+    const toy1 = {name: 'Buss myYear', price: 19};
+    const toy2 = {name: 'Rallo mcKing', price: 29};
+    console.log(toy1.price + toy2.price);
+
+    //2.4
+    let globalBasePrice = 10000;
+    const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+    const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+    globalBasePrice = 25000;
+    car1.finalPrice = car1.basePrice + globalBasePrice;
+        console.log(car1.finalPrice);
+    car2.finalPrice = car2.basePrice + globalBasePrice;
+        console.log(car2.finalPrice);
